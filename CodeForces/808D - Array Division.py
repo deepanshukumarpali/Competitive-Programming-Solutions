bcde2b39e858f09ba7fd56e7f324a7d83cd41abf
Deepanshu Kumar Pali <54951962+deepanshukumarpali@users.noreply.github.com>
2020-08-13 10:52:31 +0530

    Create 808D - Array Division.py

---
+++ CodeForces/808D - Array Division.py
@@ -0,0 +1,137 @@
+#------------------------template--------------------------#
+import os
+import sys
+from math import *
+from collections import *
+from fractions import *
+from bisect import *
+from heapq import*
+from io import BytesIO, IOBase
+def vsInput():
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+ALPHA='abcdefghijklmnopqrstuvwxyz'
+M=10**9+7
+EPS=1e-6
+def value():return tuple(map(int,input().split()))
+def array():return [int(i) for i in input().split()]
+def Int():return int(input())
+def Str():return input()
+def arrayS():return [i for i in input().split()]
+
+
+#-------------------------code---------------------------#
+# vsInput()
+
+n=Int()
+a=array()
+tot=sum(a)
+
+C=Counter(a)
+cur=defaultdict(int)
+cur[a[0]]+=1
+s1=0
+
+ok=False
+for i in range(n):
+    s2=tot-s1
+    dif=abs(s2-s1)
+    # print(dif,s1,s2)
+
+
+    if(s1==s2 and s1!=0):
+        ok=True
+        break
+
+    if(s1>s2 and cur[dif//2]>0 and not dif%2):
+        ok=True
+        break
+    if(s2>s1 and C[dif//2]-cur[dif//2]>0 and not dif%2):
+        ok=True
+        break
+
+    cur[a[i]]+=1
+    s1+=a[i]
+
+if(ok): print("YES")
+else: print("NO")
+
+
+
+
+    
+
+
+
+
+    
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+
+    
+
+
+
+
+
+
+
+
+
+    
